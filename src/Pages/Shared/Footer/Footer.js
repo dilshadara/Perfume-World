3f0216719a2c89fe84c01ba0c7af40d86ea97409
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-05-08 01:04:00 +1000

    footer completed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
+
+    const date= new Date();
+    let year = date.getFullYear();
+
     return (
-        <div>
+        <div className='footer w-100'>
+            <div>
+                <h2>NO#1 wholesaler of World's famous brand perfumes
+                    
+                </h2>
+                <p style={{textAlign:'left'}}> <small>You can trust us</small></p>
+            </div>
+            <div>
+                <p>&copy; Perfume World {year}</p>
+                <p style={{textAlign:'right'}}>All rights reserved</p>
+            </div>
             
         </div>
     );
